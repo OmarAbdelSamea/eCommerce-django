bdf44557bac828fd2f9352776ea22e1f7f7bc509
Omar Rashed <70315665+OmarRash@users.noreply.github.com>
2021-12-07 16:44:41 +0200

    database updates

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
-from product.models import Category, Product
+from product.models import *
 
 
 admin.site.register(Category)
 admin.site.register(Product)
+admin.site.register(Profile)
+admin.site.register(Transaction)
+admin.site.register(Share)
+admin.site.register(Order)
+admin.site.register(Gift)
+
--- product/models.py
+++ product/models.py
@@ -6,29 +6,33 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     cash = models.DecimalField(max_digits=7, decimal_places=2,default=0)
-#     location = models.CharField(max_length=30, blank=True)
-#     birth_date = models.DateField(null=True, blank=True)
-#     tag= models.CharField(max_length=255,unique=True,null=False)
-#     SEX_CHOICES = (
-#         ('F', 'Female',),
-#         ('M', 'Male',),
-#         )
-#     sex = models.CharField(
-#         max_length=1,
-#         choices=SEX_CHOICES,
-#     )
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    cash = models.DecimalField(max_digits=7, decimal_places=2,default=0)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    tag= models.CharField(max_length=255,unique=True,null=False)
+    SEX_CHOICES = (
+        ('F', 'Female',),
+        ('M', 'Male',),
+        )
+    sex = models.CharField(
+        max_length=1,
+        choices=SEX_CHOICES,
+    )
 
-# @receiver(post_save, sender=User)
-# def create_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
 
-# @receiver(post_save, sender=User)
-# def save_user_profile(sender, instance, **kwargs):
-#     instance.profile.save()
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 class Category(models.Model):
     name= models.CharField(max_length=255)
@@ -88,21 +92,28 @@ class Product(models.Model):
         thumbnail = File(thumb_io,name=imagemain.name)
         return thumbnail
 
-# class Order(models.Model):
-#     maker= models.ForeignKey(Profile,related_name='orders',on_delete=models.CASCADE,null=False)
-#     product = models.ForeignKey(Product,related_name='orders',on_delete=models.CASCADE,null=False)
-#     sold = models.BooleanField(default=False)
-#     amount = models.DecimalField(max_digits=3,decimal_places=0, default=1)
-
-# class Gift (models.Model):
-#     order= models.ForeignKey(Order,related_name='gift',on_delete=models.CASCADE,null=False)
-#     reciever= models.ForeignKey(Profile,related_name='gifts',on_delete=models.CASCADE,null=False)
-
-# class Transaction (models.Model):
-#     sender = models.ForeignKey(Profile,related_name= 'sent_money',on_delete=models.CASCADE,null=False)
-#     reciever = models.ForeignKey(Profile,related_name= 'recieved_money',on_delete=models.CASCADE,null=False)
-#     transaction_size = models.DecimalField(max_digits=3,decimal_places=2, default=0)
+class Order(models.Model):
+    maker= models.ForeignKey(Profile,related_name='orders',on_delete=models.CASCADE,null=False)
+    product = models.ForeignKey(Product,related_name='orders',on_delete=models.CASCADE,null=False)
+    sold = models.BooleanField(default=False)
+    amount = models.DecimalField(max_digits=3,decimal_places=0, default=1)
+    date_added = models.DateTimeField(auto_now_add=True)
     
-# class Share(models.Model):
-#     product=models.ForeignKey(Product,related_name='shared_by',on_delete=models.CASCADE,null=False)
-#     share_holder=models.ForeignKey(Profile,related_name='shared',on_delete=models.CASCADE,null=False)
+    class Meta:
+        ordering=('-date_added',)
+            
+    def __str__(self):
+        return self.id
+
+class Gift (models.Model):
+    order= models.ForeignKey(Order,related_name='gift',on_delete=models.CASCADE,null=False)
+    reciever= models.ForeignKey(Profile,related_name='gifts',on_delete=models.CASCADE,null=False)
+
+class Transaction (models.Model):
+    sender = models.ForeignKey(Profile,related_name= 'sent_money',on_delete=models.CASCADE,null=False)
+    reciever = models.ForeignKey(Profile,related_name= 'recieved_money',on_delete=models.CASCADE,null=False)
+    transaction_size = models.DecimalField(max_digits=3,decimal_places=2, default=0)
+    
+class Share(models.Model):
+    product=models.ForeignKey(Product,related_name='shared_by',on_delete=models.CASCADE,null=False)
+    share_holder=models.ForeignKey(Profile,related_name='shared',on_delete=models.CASCADE,null=False)
